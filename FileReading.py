9553ef15160ee81ce2413699eb13b6139ed13453
= <=>
2023-11-05 20:25:22 +0000

    Changes made to FileWriting.py and comments added

--- FileReading.py
+++ FileReading.py
@@ -1,3 +1,10 @@
 file = open("git_commands.txt","r")
 print(file.readlines()[2:])
 file.close()
+'''open is a function that opens a file in the folder it's in. The first parameter is the name of the file 
+and the second parameter is what you want to do with the file after opening it. In this case, the second
+parameter is r. What this means is the file is being opened for reading purposes. This means the only 
+thing you can do with this file after opening it is read it.'''
+'''print(file.readlines()[2:]) - what this does is print out part of the file. The parts of the file that
+are printed out can be set using () and []. In this case, there is nothing in () and 2: in []. What this
+does is print out the entire file apart from 2(indexing starts at 0 so this would be the third line)'''
--- FileWriting.py
+++ FileWriting.py
@@ -1,3 +1,7 @@
-writeToFile = open("git_commands.txt","a")
-writeToFile.write("git push origin branch_name()")
-
+'''writeToFile = open("git_commands.txt","w")
+writeToFile.write("\ngit push origin branch_name - pushes the code to a specified branch in git")
+writeToFile.close()'''
+writeToFile = open("pokemon_names.txt","x")
+writeToFile.write("Bidoof")
+writeToFile.write("\nCofargrigus")
+writeToFile.write("\nMedicham")
